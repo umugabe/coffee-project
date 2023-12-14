c6ad4de4ec463608fad9c9fe9caa1c595d82e71f
celine123-code <mumararungucelyne@gmail.com>
2023-12-14 10:25:11 -0800

    adding

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+let typed = new Typed('.auto-input', {
+    strings: ['WELCOME TO NYANYUMBA','WOMENS', 'SPECIALIST COFEE'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+});
